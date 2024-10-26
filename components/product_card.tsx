9d947a5f79138db3938829c84717d323eca9c5f3
Corkichan <roobick@outlook.com>
2024-10-26 16:41:49 +0700

    Fix Card

--- components/product_card.tsx
+++ components/product_card.tsx
@@ -11,14 +11,16 @@ type CardProps = {
 export default function Card({ title, description, imageUrl, warehouse, amount }: CardProps) {
   return (
     <div 
-      className="flex flex-col-1 h-44 w-auto rounded overflow-hidden shadow-lg bg-red-500 cursor-pointer hover:shadow-xl transition-shadow duration-300"
+      className="flex flex-col w-full max-w-xs h-auto rounded overflow-hidden shadow-lg bg-red-500 cursor-pointer hover:shadow-xl transition-shadow duration-300"
     >
-      <img className="w-44 h-44 object-fit" src={imageUrl} alt={title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-white whitespace-nowrap overflow-auto ">{title}</div>
-        <p className="text-white text-base">{description}</p>
-        <p className="text-white">Warehouse: {warehouse}</p>
-        <p className="text-white">Amount: {amount}</p>
+      <div className="w-48 aspect-w-4 aspect-h-3">
+        <img className="w-full h-full object-cover" src={imageUrl} alt={title} />
+      </div>
+      <div className="px-4 py-2 flex flex-col h-full"> {/* Ensure vertical layout */}
+        <div className="font-bold text-sm mb-1 text-white truncate">{title}</div>
+        <p className="text-white text-xs mb-1 line-clamp-2">{description}</p>
+        <p className="text-white text-xs">Warehouse: {warehouse}</p>
+        <p className="text-white text-xs">Amount: {amount}</p>
       </div>
     </div>
   );
